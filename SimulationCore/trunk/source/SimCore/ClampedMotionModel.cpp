8d79710f96edaa7d4155ee10e794509ec0c53479
legregius <legregius@df8a077f-1820-0410-8776-e0b621f843e8>
2007-08-29 14:01:23 +0000

    fixed an compile error that didn't give me a compile error?  Very very strange.

--- SimulationCore/trunk/source/SimCore/ClampedMotionModel.cpp
+++ SimulationCore/trunk/source/SimCore/ClampedMotionModel.cpp
@@ -33,221 +33,215 @@
 
 namespace SimCore
 {
+   IMPLEMENT_MANAGEMENT_LAYER(ClampedMotionModel);
    
-   namespace Components
+   //////////////////////////////////////////////////////////////////////////         
+   // Passenger Motion Model Code
+   //////////////////////////////////////////////////////////////////////////         
+   ClampedMotionModel::ClampedMotionModel(dtCore::Keyboard* keyboard,
+                                    dtCore::Mouse* mouse) 
+      : AttachedMotionModel(keyboard,mouse),
+      mFreeLookKey(Producer::Key_Control_L),
+      mFreeLookMouseButton(dtCore::Mouse::RightButton),
+      mFreeLookByKey(false),
+      mFreeLookByMouseButton(false),
+      mFreeLookWasHeld(false),
+      mFreeLookMouseButtonWasHeld(false),
+      mRecenterMouse(false),
+      mResetRotation(false),
+      mReverseLeftRight(false),
+      mReverseUpDown(false),
+      mLeftRightLimit(-1.0),
+      mUpDownLimit(-1.0),
+      mKeyboard(keyboard),
+      mDOF(NULL)
    {
-      
-      IMPLEMENT_MANAGEMENT_LAYER(ClampedMotionModel);
-      
-      //////////////////////////////////////////////////////////////////////////         
-      // Passenger Motion Model Code
-      //////////////////////////////////////////////////////////////////////////         
-      ClampedMotionModel::ClampedMotionModel(dtCore::Keyboard* keyboard,
-                                       dtCore::Mouse* mouse) 
-         : AttachedMotionModel(keyboard,mouse),
-         mFreeLookKey(Producer::Key_Control_L),
-         mFreeLookMouseButton(dtCore::Mouse::RightButton),
-         mFreeLookByKey(false),
-         mFreeLookByMouseButton(false),
-         mFreeLookWasHeld(false),
-         mFreeLookMouseButtonWasHeld(false),
-         mRecenterMouse(false),
-         mResetRotation(false),
-         mReverseLeftRight(false),
-         mReverseUpDown(false),
-         mLeftRightLimit(-1.0),
-         mUpDownLimit(-1.0),
-         mKeyboard(keyboard),
-         mDOF(NULL)
-      {
-      }
+   }
 
-      //////////////////////////////////////////////////////////////////////////         
-      ClampedMotionModel::~ClampedMotionModel()
-      {
-      }
+   //////////////////////////////////////////////////////////////////////////         
+   ClampedMotionModel::~ClampedMotionModel()
+   {
+   }
 
-      //////////////////////////////////////////////////////////////////////////         
-      void ClampedMotionModel::OnMessage(MessageData *data)
+   //////////////////////////////////////////////////////////////////////////         
+   void ClampedMotionModel::OnMessage(MessageData *data)
+   {
+      if(data->message == "preframe" && (GetTarget() != NULL || GetTargetDOF() != NULL) && IsEnabled())
       {
-         if(data->message == "preframe" && (GetTarget() != NULL || GetTargetDOF() != NULL) && IsEnabled())
+         if( ! IsFreeLookHeld() )
          {
-            if( ! IsFreeLookHeld() )
+            if( mResetRotation )
             {
-               if( mResetRotation )
-               {
-                  SetTargetsRotation(mRestingRotation);
-               }
-
-               GetLeftRightMouseAxis()->SetState(0.0);
-               GetUpDownMouseAxis()->SetState(0.0);
-               if( mRecenterMouse )
-               {
-                  GetMouse()->SetPosition(0.0f,0.0f);//keeps cursor at center of screen
-               }
+               SetTargetsRotation(mRestingRotation);
             }
-            else
+
+            GetLeftRightMouseAxis()->SetState(0.0);
+            GetUpDownMouseAxis()->SetState(0.0);
+            if( mRecenterMouse )
             {
-               // Reset the mouse position back to the center
-               if( mRecenterMouse )
-               {
-                  GetMouse()->SetPosition(0.0f,0.0f);
-               }
-
-               // Do the original rotation
-               //get the real time.
-               const double deltaFrameTime = static_cast<const double*>(data->userData)[1];
-
-               osg::Vec3 hpr = GetTargetsRotation();
-               mHPRChange = hpr;
-
-               bool setHPR = false;
-
-               if (GetLeftRightMouseAxis()->GetState() != 0)
-               {
-                  setHPR = true;
-                  double change = GetLeftRightMouseAxis()->GetState() * GetMaximumMouseTurnSpeed() * deltaFrameTime;
-
-                  //std::cout << "Change LR: " << GetLeftRightMouseAxis()->GetState() << std::endl;
-                  //prevent huge jumps.
-                  dtUtil::Clamp(change, -15.0, 15.0);
-                  hpr[0] -= mReverseLeftRight ? -change : change;
-                  GetLeftRightMouseAxis()->SetState(0.0);//necessary to stop camera drifting down
-                  GetMouse()->SetPosition(0.0f,0.0f);//keeps cursor at center of screen
-               }
-
-               if (GetUpDownMouseAxis()->GetState() != 0)
-               {
-                  setHPR = true;
-                  double change = GetUpDownMouseAxis()->GetState() * GetMaximumMouseTurnSpeed() * deltaFrameTime;
-
-                  //prevent huge jumps.
-                  //std::cout << "Change UD: " << GetUpDownMouseAxis()->GetState() << std::endl;
-                  dtUtil::Clamp(change, -15.0, 15.0);
-                  hpr[1] += mReverseUpDown ? -change : change;
-                  GetUpDownMouseAxis()->SetState(0.0);//necessary to stop camera drifting down
-                  GetMouse()->SetPosition(0.0f,0.0f);//keeps cursor at center of screen
-               }
-
-               // Clamp rotation
-               if( mUpDownLimit >= 0.0 || mLeftRightLimit >= 0.0 )
-               {
-                  // Use a local variable to get around Vec3 Real-Type ambiguity (double vs. float)
-                  double value;
-
-                  // Clamp up-down
-                  if( mUpDownLimit >= 0.0 )
-                  {
-                     value = hpr[1];
-                     dtUtil::Clamp( value, -mUpDownLimit, mUpDownLimit );
-                     hpr[1] = value;
-                  }
-
-                  // Clamp left-right
-                  if( mLeftRightLimit >= 0.0 )
-                  {
-                     value = hpr[0];
-                     dtUtil::Clamp( value, -mLeftRightLimit, mLeftRightLimit );
-                     hpr[0] = value;
-                  }
-               }
-
-               if(setHPR)
-               {
-                  hpr[2] = 0.0f;
-                  SetTargetsRotation(hpr);                  
-                  //GetMouse()->SetPosition(0.0f,0.0f);//keeps cursor at center of screen
-               }
-
-               // Get the current change in orientation
-               mHPRChange = hpr - mHPRChange;
-               
-               if( mArticHelper.valid() )
-               {
-                  if( mDOF.valid() )
-                  {
-                     mArticHelper->HandleUpdatedDOFOrientation( *mDOF, mHPRChange, hpr );
-                  }
-               }
-
-         }
-      
-         }
-      }
-
-      //////////////////////////////////////////////////////////////////////////         
-      void ClampedMotionModel::SetTargetsRotation(const osg::Vec3 &newHpr)
-      {
-         // the motion model is working directly against the drawable
-         if (!mDOF.valid() && GetTarget() != NULL)
-         {
-            dtCore::Transform transform;
-            GetTarget()->GetTransform(transform, dtCore::Transformable::REL_CS);
-            transform.SetRotation( newHpr );
-            GetTarget()->SetTransform(transform, dtCore::Transformable::REL_CS);
-         }
-         // the motion model is working against the child dof 
-         else if (mDOF.valid())
-         {
-            mDOF->setCurrentHPR(newHpr * 0.0174533); // convert degrees to radians
-         }
-      }
-
-      //////////////////////////////////////////////////////////////////////////         
-      osg::Vec3 ClampedMotionModel::GetTargetsRotation() const
-      {
-         // the motion model is working directly against the drawable
-         if (!mDOF.valid() && GetTarget() != NULL)
-         {
-            dtCore::Transform transform;
-            osg::Vec3 hpr;
-            GetTarget()->GetTransform(transform, dtCore::Transformable::REL_CS);
-            transform.GetRotation(hpr);
-            return hpr;
-         }
-         // the motion model is working against the child dof 
-         else if (mDOF.valid())
-         {
-            return mDOF->getCurrentHPR() * 57.29578;
+               GetMouse()->SetPosition(0.0f,0.0f);//keeps cursor at center of screen
+            }
          }
          else
          {
-            osg::Vec3 result;
-            return result;
-         }
-      }
+            // Reset the mouse position back to the center
+            if( mRecenterMouse )
+            {
+               GetMouse()->SetPosition(0.0f,0.0f);
+            }
 
-      //////////////////////////////////////////////////////////////////////////         
-      bool ClampedMotionModel::IsKeyHeld() const
+            // Do the original rotation
+            //get the real time.
+            const double deltaFrameTime = static_cast<const double*>(data->userData)[1];
+
+            osg::Vec3 hpr = GetTargetsRotation();
+            mHPRChange = hpr;
+
+            bool setHPR = false;
+
+            if (GetLeftRightMouseAxis()->GetState() != 0)
+            {
+               setHPR = true;
+               double change = GetLeftRightMouseAxis()->GetState() * GetMaximumMouseTurnSpeed() * deltaFrameTime;
+
+               //std::cout << "Change LR: " << GetLeftRightMouseAxis()->GetState() << std::endl;
+               //prevent huge jumps.
+               dtUtil::Clamp(change, -15.0, 15.0);
+               hpr[0] -= mReverseLeftRight ? -change : change;
+               GetLeftRightMouseAxis()->SetState(0.0);//necessary to stop camera drifting down
+               GetMouse()->SetPosition(0.0f,0.0f);//keeps cursor at center of screen
+            }
+
+            if (GetUpDownMouseAxis()->GetState() != 0)
+            {
+               setHPR = true;
+               double change = GetUpDownMouseAxis()->GetState() * GetMaximumMouseTurnSpeed() * deltaFrameTime;
+
+               //prevent huge jumps.
+               //std::cout << "Change UD: " << GetUpDownMouseAxis()->GetState() << std::endl;
+               dtUtil::Clamp(change, -15.0, 15.0);
+               hpr[1] += mReverseUpDown ? -change : change;
+               GetUpDownMouseAxis()->SetState(0.0);//necessary to stop camera drifting down
+               GetMouse()->SetPosition(0.0f,0.0f);//keeps cursor at center of screen
+            }
+
+            // Clamp rotation
+            if( mUpDownLimit >= 0.0 || mLeftRightLimit >= 0.0 )
+            {
+               // Use a local variable to get around Vec3 Real-Type ambiguity (double vs. float)
+               double value;
+
+               // Clamp up-down
+               if( mUpDownLimit >= 0.0 )
+               {
+                  value = hpr[1];
+                  dtUtil::Clamp( value, -mUpDownLimit, mUpDownLimit );
+                  hpr[1] = value;
+               }
+
+               // Clamp left-right
+               if( mLeftRightLimit >= 0.0 )
+               {
+                  value = hpr[0];
+                  dtUtil::Clamp( value, -mLeftRightLimit, mLeftRightLimit );
+                  hpr[0] = value;
+               }
+            }
+
+            if(setHPR)
+            {
+               hpr[2] = 0.0f;
+               SetTargetsRotation(hpr);                  
+               //GetMouse()->SetPosition(0.0f,0.0f);//keeps cursor at center of screen
+            }
+
+            // Get the current change in orientation
+            mHPRChange = hpr - mHPRChange;
+            
+            if( mArticHelper.valid() )
+            {
+               if( mDOF.valid() )
+               {
+                  mArticHelper->HandleUpdatedDOFOrientation( *mDOF, mHPRChange, hpr );
+               }
+            }
+
+      }
+   
+      }
+   }
+
+   //////////////////////////////////////////////////////////////////////////         
+   void ClampedMotionModel::SetTargetsRotation(const osg::Vec3 &newHpr)
+   {
+      // the motion model is working directly against the drawable
+      if (!mDOF.valid() && GetTarget() != NULL)
       {
-         return mKeyboard->GetKeyState( mFreeLookKey );
+         dtCore::Transform transform;
+         GetTarget()->GetTransform(transform, dtCore::Transformable::REL_CS);
+         transform.SetRotation( newHpr );
+         GetTarget()->SetTransform(transform, dtCore::Transformable::REL_CS);
       }
-
-      //////////////////////////////////////////////////////////////////////////         
-      bool ClampedMotionModel::IsMouseButtonHeld() const
+      // the motion model is working against the child dof 
+      else if (mDOF.valid())
       {
-         return GetMouse() != NULL ? GetMouse()->GetButtonState( mFreeLookMouseButton ) : false;
+         mDOF->setCurrentHPR(newHpr * 0.0174533); // convert degrees to radians
       }
+   }
 
-      //////////////////////////////////////////////////////////////////////////         
-      bool ClampedMotionModel::IsFreeLookHeld()
+   //////////////////////////////////////////////////////////////////////////         
+   osg::Vec3 ClampedMotionModel::GetTargetsRotation() const
+   {
+      // the motion model is working directly against the drawable
+      if (!mDOF.valid() && GetTarget() != NULL)
       {
-         if( ! mFreeLookByKey && ! mFreeLookByMouseButton ) { return true; }
-
-         if( mFreeLookByKey && ! mFreeLookByMouseButton )
-         {
-            return IsKeyHeld();
-         }
-         else if( ! mFreeLookByKey && mFreeLookByMouseButton )
-         {
-            return IsMouseButtonHeld();
-         }
-         else if( mFreeLookByKey && mFreeLookByMouseButton )
-         {
-            return IsKeyHeld() || IsMouseButtonHeld();
-         }
-         return false;
+         dtCore::Transform transform;
+         osg::Vec3 hpr;
+         GetTarget()->GetTransform(transform, dtCore::Transformable::REL_CS);
+         transform.GetRotation(hpr);
+         return hpr;
       }
+      // the motion model is working against the child dof 
+      else if (mDOF.valid())
+      {
+         return mDOF->getCurrentHPR() * 57.29578;
+      }
+      else
+      {
+         osg::Vec3 result;
+         return result;
+      }
+   }
 
+   //////////////////////////////////////////////////////////////////////////         
+   bool ClampedMotionModel::IsKeyHeld() const
+   {
+      return mKeyboard->GetKeyState( mFreeLookKey );
+   }
+
+   //////////////////////////////////////////////////////////////////////////         
+   bool ClampedMotionModel::IsMouseButtonHeld() const
+   {
+      return GetMouse() != NULL ? GetMouse()->GetButtonState( mFreeLookMouseButton ) : false;
+   }
+
+   //////////////////////////////////////////////////////////////////////////         
+   bool ClampedMotionModel::IsFreeLookHeld()
+   {
+      if( ! mFreeLookByKey && ! mFreeLookByMouseButton ) { return true; }
+
+      if( mFreeLookByKey && ! mFreeLookByMouseButton )
+      {
+         return IsKeyHeld();
+      }
+      else if( ! mFreeLookByKey && mFreeLookByMouseButton )
+      {
+         return IsMouseButtonHeld();
+      }
+      else if( mFreeLookByKey && mFreeLookByMouseButton )
+      {
+         return IsKeyHeld() || IsMouseButtonHeld();
+      }
+      return false;
    }
 }
