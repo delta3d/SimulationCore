a05ccd9ab430daf05edad709ae5644d64c20e149
legregius <legregius@df8a077f-1820-0410-8776-e0b621f843e8>
2007-09-04 16:22:17 +0000

    fixed a problem with a catch clause logging an exception, not failing the test.

--- SimulationCore/trunk/source/SimCore/Actors/TerrainActorProxy.cpp
+++ SimulationCore/trunk/source/SimCore/Actors/TerrainActorProxy.cpp
@@ -99,7 +99,7 @@ namespace SimCore
             if (!fileName.empty())
             {
                dtCore::RefPtr<osg::Node> unused;
-               if( !IGActor::LoadFile(fileName, mTerrainNode, unused, false, true) )
+               if( !IGActor::LoadFile(fileName, mTerrainNode, unused, true, true) )
                   LOG_ERROR("Failed to load the terrain file: " + fileName);
 
                //mTerrainNode = IGActor::LoadFile(fileName, false);
