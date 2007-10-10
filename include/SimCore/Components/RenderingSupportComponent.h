8bc35389b60659db2e72b000df4a999ae0b1700f
banderegg <banderegg@df8a077f-1820-0410-8776-e0b621f843e8>
2007-10-10 16:03:15 +0000

    +added a render bin for the terrain to render it after all the non transparent entities

--- include/SimCore/Components/RenderingSupportComponent.h
+++ include/SimCore/Components/RenderingSupportComponent.h
@@ -205,6 +205,7 @@ namespace SimCore
             //here we define constants for defining the render bins
             //so we don't have hard coded render bin numbers all over the place
             static const int RENDER_BIN_ENVIRONMENT       = -2;
+            static const int RENDER_BIN_TERRAIN           = 5;
             static const int RENDER_BIN_SKY_AND_ATMOSPHERE=  9;
             static const int RENDER_BIN_PRECIPITATION     = 11;
             static const int RENDER_BIN_TRANSPARENT       = 10;
