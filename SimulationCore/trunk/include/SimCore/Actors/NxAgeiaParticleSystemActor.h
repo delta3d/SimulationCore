d4b6c3e7a3790d3cf97b2c5096752c3ad533c76a
adanklefsen <adanklefsen@df8a077f-1820-0410-8776-e0b621f843e8>
2007-11-13 17:59:46 +0000

    Reverted these two files, the changes were not needed.

--- SimulationCore/trunk/include/SimCore/Actors/NxAgeiaParticleSystemActor.h
+++ SimulationCore/trunk/include/SimCore/Actors/NxAgeiaParticleSystemActor.h
@@ -143,8 +143,6 @@ class SIMCORE_EXPORT NxAgeiaParticleSystemActor: public dtGame::GameActor, publi
 
    public:
 
-      void LoadPhysicsParticleFile(const std::string& fileString);
-
       /// turn it on or off from spawning...
       void ToggleEmitter(bool value) {mIsCurrentlyOn = value; mSystemsTimeTotalTimeLength = 0;}
       
