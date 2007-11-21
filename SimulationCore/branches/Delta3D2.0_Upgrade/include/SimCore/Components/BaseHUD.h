a789a9e3838b3ea21f8e06725dc5b3efd1cfc352
fretlessmayhem <fretlessmayhem@df8a077f-1820-0410-8776-e0b621f843e8>
2007-11-21 19:08:23 +0000

    Had to tweak the HUD code slightly in order for it to play nice with Delta3D 2.0

--- SimulationCore/branches/Delta3D2.0_Upgrade/include/SimCore/Components/BaseHUD.h
+++ SimulationCore/branches/Delta3D2.0_Upgrade/include/SimCore/Components/BaseHUD.h
@@ -114,6 +114,10 @@ namespace SimCore
 
          virtual void RegisterCallbacks();
 
+         void InitializeCEGUI();
+
+         virtual void OnEnteredWorld();
+
       private:
          dtCore::DeltaWin *mWin;
          dtCore::RefPtr<HUDGroup> mMainWindow;
@@ -121,6 +125,8 @@ namespace SimCore
          dtGUI::BaseScriptModule* mScriptModule;
 
          HUDState *mHUDState;
+
+         std::string mSchemeFile;
       };
 
    }
