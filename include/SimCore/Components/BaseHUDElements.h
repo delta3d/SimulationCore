515c39bf7308c3c1127014f0751bb2bef7cf3a34
crodgers <crodgers@df8a077f-1820-0410-8776-e0b621f843e8>
2007-12-07 17:34:17 +0000

    Added an empty virtual method, UpdateLayout, to the base HUDElement class.
    Also change the implementations of Hide and Show to call SetVisible for changing visibility state of a HUD element.

--- include/SimCore/Components/BaseHUDElements.h
+++ include/SimCore/Components/BaseHUDElements.h
@@ -132,7 +132,11 @@ namespace SimCore
             bool IsAbsolutePosition() const { return mAbsPos; }
             bool IsAbsoluteSize() const { return mAbsSize; }
 
-            // Utility funtion
+            // Override this if the element is complex and
+            // needs to update child elements
+            virtual void UpdateLayout() {}
+
+            // Utility function
             void SetCEGUIImage( CEGUI::Window& window, 
                const std::string& imagesetName,
                const std::string& imageName,
