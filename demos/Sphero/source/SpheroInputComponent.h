22f4f5c2f8178555b3755bc16374aedbab9fea1e
David Guthrie <LEgregius@users.noreply.github.com>
2008-06-06 15:09:38 +0000

    missed the source, duh

---
+++ demos/Sphero/source/SpheroInputComponent.h
@@ -0,0 +1,75 @@
+/* -*-c++-*-
+ * Delta3D Open Source Game and Simulation Engine
+ * Copyright (C) 2008, Alion Science and Technology
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * David Guthrie
+ */
+#ifndef PRIMITIVE_EXAMPLE_INPUT_COMPONENT
+#define PRIMITIVE_EXAMPLE_INPUT_COMPONENT
+
+#include "export.h"
+#include <SimCore/Components/BaseInputComponent.h>
+#include <dtCore/flymotionmodel.h>
+#include <dtCore/refptr.h>
+#include <dtPhysics/physicshelper.h>
+
+namespace Sphero
+{
+   class SPHERO_EXPORT SpheroInputComponent : public SimCore::Components::BaseInputComponent
+   {
+      public:
+         typedef SimCore::Components::BaseInputComponent BaseClass;
+         
+         /// Constructor
+         SpheroInputComponent(const std::string& name = SimCore::Components::BaseInputComponent::DEFAULT_NAME);
+
+         virtual void ProcessMessage(const dtGame::Message& message);
+
+         /**
+          *
+          */
+         virtual void OnAddedToGM();
+
+         /**
+          * Called when this component is removed from the GM
+          */
+         virtual void OnRemovedFromGM();
+
+         /**
+          * KeyboardListener call back- Called when a key is pressed.
+          * Override this if you want to handle this listener event.
+          * Default handles the Escape key to quit.
+          *
+          * @param keyboard the source of the event
+          * @param key the key pressed
+          * @param character the corresponding character
+          */
+         virtual bool HandleKeyPressed(const dtCore::Keyboard* keyboard, int key);
+
+      protected:
+         virtual ~SpheroInputComponent();
+         void FireSomething();
+         void SetupMaterialsAndTerrain();
+         void UpdateHelpers();
+      private:
+         typedef std::vector<dtCore::RefPtr<dtPhysics::PhysicsHelper> > HelperList;
+         dtCore::RefPtr<dtCore::FlyMotionModel> mMotionModel;
+         HelperList mHelpers;
+   };
+}
+
+#endif
