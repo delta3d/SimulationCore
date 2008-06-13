146de6c3ffa6d94566018ee6e851bcf872ea84b4
cmmurphy <cmmurphy@df8a077f-1820-0410-8776-e0b621f843e8>
2008-06-13 16:32:29 +0000

    Updates the version.

--- SimulationCore/branches/Alion_08_05_01_DVTE08_Bugfix_RC/include/SimCore/SimCoreVersion.h
+++ SimulationCore/branches/Alion_08_05_01_DVTE08_Bugfix_RC/include/SimCore/SimCoreVersion.h
@@ -38,9 +38,9 @@
 
 namespace SimCore
 {
-   std::string SIMCORE_SVN_REVISION("626");
-   std::string SIMCORE_SVN_DATE("2008/05/23 13:11:51");
-   std::string SIMCORE_BUILD_DATE("2008/05/23 14:59:55");
+   std::string SIMCORE_SVN_REVISION("666");
+   std::string SIMCORE_SVN_DATE("2008/06/13 12:25:55");
+   std::string SIMCORE_BUILD_DATE("2008/06/13 12:27:46");
 }
 
 #endif
