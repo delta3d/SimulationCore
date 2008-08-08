1291f1ee7513a38033c37d0a539dc239d319041e
cmmurphy <cmmurphy@df8a077f-1820-0410-8776-e0b621f843e8>
2008-08-08 21:45:07 +0000

    Replaced the distribution license on the unit tests, the test apps, the demos, and STAGE. Also, the demos such as Driver Demo and the Qt part of the Stealth Viewer as well.

--- SimulationCore/trunk/tests/SimCore/HUDElementsTests.cpp
+++ SimulationCore/trunk/tests/SimCore/HUDElementsTests.cpp
@@ -1,16 +1,30 @@
-/*
- * Copyright, 2006, Alion Science and Technology Corporation, all rights reserved.
- * 
- * Alion Science and Technology Corporation
- * 5365 Robin Hood Road
- * Norfolk, VA 23513
- * (757) 857-5670, www.alionscience.com
- * 
- * This software was developed by Alion Science and Technology Corporation under
- * circumstances in which the U. S. Government may have rights in the software.
- *
- * @author Chris Rodgers
- */
+/* -*-c++-*-
+* Simulation Core - HUDElementsTests (.h & .cpp) - Using 'The MIT License'
+* Copyright (C) 2006-2008, Alion Science and Technology Corporation
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+* 
+* This software was developed by Alion Science and Technology Corporation under
+* circumstances in which the U. S. Government may have rights in the software.
+*
+* @author Chris Rodgers
+*/
 #include <prefix/SimCorePrefix-src.h>
 #include <cppunit/extensions/HelperMacros.h>
 
