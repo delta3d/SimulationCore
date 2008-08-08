3b6aa786f8b39c5de380089241d686d0c813e990
David Guthrie <LEgregius@users.noreply.github.com>
2008-08-08 19:18:15 +0000

    fixed a test with a messed up path and parameter count.  It wasn't really breaking anything, but I noticed it was wrong.

--- tests/StealthViewer/StealthViewerHLAWindowTests.cpp
+++ tests/StealthViewer/StealthViewerHLAWindowTests.cpp
@@ -16,7 +16,7 @@
 
 #include <UnitTestMain.h>
 
-class StealthViewerHLAWindowTests : public CPPUNIT_NS::TestFixture 
+class StealthViewerHLAWindowTests : public CPPUNIT_NS::TestFixture
 {
    CPPUNIT_TEST_SUITE(StealthViewerHLAWindowTests);
 
@@ -46,14 +46,14 @@ void StealthViewerHLAWindowTests::setUp()
    std::string dir = dtUtil::FileUtils::GetInstance().CurrentDirectory();
 
 #if defined (_MSC_VER) && defined (_DEBUG)
-   dir += "bin/StealthViewerHLAWindowTestsd.exe";
+   dir += "/bin/StealthViewerHLAWindowTestsd.exe";
 #elif defined (_MSC_VER)
-   dir += "bin/StealthViewerHLAWindowTests.exe";
+   dir += "/bin/StealthViewerHLAWindowTests.exe";
 #else
-   dir += "bin/StealthViewerHLAWindowTests";
+   dir += "/bin/StealthViewerHLAWindowTests";
 #endif
 
-   int numParams = 1;
+   int numParams = 0;
    const char *exe = dir.c_str();
 
    mQApp = new QApplication(numParams, const_cast<char**>(&exe));
@@ -79,10 +79,10 @@ void StealthViewerHLAWindowTests::TestDisplaySavedConnections()
    settings.ClearAllSettings(true);
 
    StealthQt::HLAWindow *window = new StealthQt::HLAWindow(*mGM, NULL, &settings);
-   
+
    QListWidgetItem *shouldBeNULL = window->GetNetworkListWidget()->item(0);
 
-   CPPUNIT_ASSERT_MESSAGE("Initially, the widget should be empty", 
+   CPPUNIT_ASSERT_MESSAGE("Initially, the widget should be empty",
        shouldBeNULL == NULL);
 
    delete window;
@@ -101,7 +101,7 @@ void StealthViewerHLAWindowTests::TestDisplaySavedConnections()
    testProps.push_back(QString("TestFedName"));
    testProps.push_back(QString("TestRidFile"));
 
-   settings.AddConnection(testProps[0], testProps[1], testProps[2], 
+   settings.AddConnection(testProps[0], testProps[1], testProps[2],
       testProps[3], testProps[4], testProps[5], testProps[6]);
 
    CPPUNIT_ASSERT_EQUAL((unsigned int)(1), settings.GetNumConnections());
@@ -117,7 +117,7 @@ void StealthViewerHLAWindowTests::TestDisplaySavedConnections()
 
    CPPUNIT_ASSERT_MESSAGE("Now that a settings file has been created with data\
                           written to it, the list widget should contain the name\
-                          of the object written.", 
+                          of the object written.",
                           item->text() == QString("TestName"));
 
    settings.ClearAllSettings(true);
@@ -128,7 +128,7 @@ void StealthViewerHLAWindowTests::TestDisplaySavedConnections()
    window = new StealthQt::HLAWindow(*mGM, NULL, &settings);
 
    count = window->GetNetworkListWidget()->count();
-   CPPUNIT_ASSERT_EQUAL_MESSAGE("The number of items in the widget should default to 0", 
+   CPPUNIT_ASSERT_EQUAL_MESSAGE("The number of items in the widget should default to 0",
                                  0, count);
 
    item = window->GetNetworkListWidget()->item(0);
