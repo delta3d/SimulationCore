4a20fd58df1063750b1ba394cf3802b7e96f4251
banderegg <banderegg@df8a077f-1820-0410-8776-e0b621f843e8>
2008-11-24 20:59:18 +0000

    +moved the texture waves back onto the cpu

--- source/SimCore/Actors/WaterGridActor.cpp
+++ source/SimCore/Actors/WaterGridActor.cpp
@@ -166,7 +166,7 @@ namespace SimCore
 
       ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
       //const int WaterGridActor::MAX_WAVES(8);
-      const int WaterGridActor::MAX_TEXTURE_WAVES(16);
+      const int WaterGridActor::MAX_TEXTURE_WAVES(32);
       const dtUtil::RefString WaterGridActor::UNIFORM_ELAPSED_TIME("elapsedTime");
       const dtUtil::RefString WaterGridActor::UNIFORM_MAX_COMPUTED_DISTANCE("maxComputedDistance");
       const dtUtil::RefString WaterGridActor::UNIFORM_WAVE_ARRAY("waveArray");
@@ -543,27 +543,39 @@ namespace SimCore
 
 
 
-         float waveDirArray[] = {10.33, 3.07, 7.0, 10.246,
-            33.0, 6.71, -104.0, 5.15,
-            -15.5, -21.0, 49.0, 11.0,
-            -9.0, -8.0, 16.113, 26.0};
+         const float kArray[] = {1.33, 1.76, 2.0, 2.246,
+            1.0, 1.71, 1.0, 1.75,
+            1.5, 1.0, 1.0, 2.0,
+            2.2, 2.0, 1.113, 1.0,
+            1.33, 1.76, 3.0, 2.246,
+            1.0, 1.71, 1.0, 1.75,
+            1.5, 1.0, 1.0, 2.0,
+            1.9, 2.0, 1.113, 1.0};
 
-         float waveLengthArray[] = {0.1788, 0.0535, 0.12186, 0.24,
-            0.14, 0.116844, 0.97437, 0.0805,
-            0.067, 0.3565, 0.67135 , 0.191,
-            0.155, 0.13917, 0.275, .448};
+         const float waveLengthArray[] = {0.218, 0.1535, 0.16186, 0.24,
+            0.19, 0.186844, 0.23437, 0.1805,
+            0.217, 0.2565, 0.17135 , 0.191,
+            0.185, 0.23917, 0.215, .248,
+            0.1788, 0.1835, 0.18186, 0.29,
+            0.22, 0.246844, 0.27437, 0.1805,
+            0.197, 0.2565, 0.2735 , 0.291,
+            0.214, 0.19917, 0.215, .248};
 
-         float waveSpeedArray[] = {0.0953, 0.03839, 0.0311, 0.04221,
-            0.11497, 0.143213, 0.14571, 0.051181,
+         const float waveSpeedArray[] = {0.0953, 0.083839, -0.0811, 0.08221,
+            -0.11497, 0.143213, 0.14571, 0.091181,
 
-            0.01473, 0.1531, 0.2131, 0.0221,
-            0.121497, 0.1213, 0.14571, 0.1181};    
+            0.08473, 0.1331, -0.1731, 0.1021,
+            0.121497, 0.1313, 0.14571, 0.1181,
+            -0.1753, 0.09839, -0.1311, 0.09821,
+            0.09497, 0.093213, 0.13571, 0.121181,
 
+            -0.10473, 0.0931, 0.13131, -0.0921,
+            0.11497, -0.1313, 0.12571, -0.0931};  
 
-         for(int i = 0; i < 16; ++i)
+         for(int i = 0; i < MAX_TEXTURE_WAVES; ++i)
          {
             TextureWave tw;
-            tw.mDirection = waveDirArray[i];
+            tw.mSteepness = kArray[i];
             tw.mSpeed = waveSpeedArray[i];
             tw.mWaveLength = waveLengthArray[i];
 
@@ -979,7 +991,7 @@ namespace SimCore
 
                float freq = (2.0f * osg::PI) / wave.mWaveLength;
 
-               textureWaveArray->setElement(count, osg::Vec4(wave.mWaveLength, wave.mSpeed, wave.mDirection, freq));
+               textureWaveArray->setElement(count, osg::Vec4(wave.mWaveLength, wave.mSpeed, wave.mSteepness, freq));
 
                ++tw_iter;
             }
