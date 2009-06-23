90ea3f3ab86cf1d1a929ed16e1b4b34121424c8a
banderegg <banderegg@df8a077f-1820-0410-8776-e0b621f843e8>
2009-06-23 11:21:51 +0000

    +adding EnemyHelix +adding EnemyDescriptionActor +adding SpawnVolumeActor +adding SpawnComponent

---
+++ demos/NetDemo/include/Components/SpawnComponent.h
@@ -0,0 +1,70 @@
+/* -*-c++-*-
+* Delta3D Open Source Game and Simulation Engine
+* Copyright (C) 2009, Alion Science and Technology, BMH Operation
+*
+* This library is free software; you can redistribute it and/or modify it under
+* the terms of the GNU Lesser General Public License as published by the Free
+* Software Foundation; either version 2.1 of the License, or (at your option)
+* any later version.
+*
+* This library is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+* FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+* details.
+*
+* You should have received a copy of the GNU Lesser General Public License
+* along with this library; if not, write to the Free Software Foundation, Inc.,
+* 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*
+* Bradley Anderegg
+*/
+#ifndef NETDEMO_SPAWNCOMPONENT
+#define NETDEMO_SPAWNCOMPONENT
+
+#include <DemoExport.h>
+#include <dtGame/gmcomponent.h>
+
+#include <Actors/SpawnVolumeActor.h>
+
+namespace NetDemo
+{
+
+   class NETDEMO_EXPORT SpawnComponent : public dtGame::GMComponent
+   {
+   public:
+
+      typedef dtGame::GMComponent BaseClass;
+      static const std::string DEFAULT_NAME;
+
+      /// Constructor
+      SpawnComponent(const std::string& name = DEFAULT_NAME);
+
+      /**
+      * Handles incoming messages
+      */
+      virtual void ProcessMessage(const dtGame::Message& message);
+
+      /**
+      * Called when this component is added to the game manager
+      */
+      virtual void OnAddedToGM();
+
+      virtual void OnRemovedFromGM();
+
+
+   protected:
+
+      /// Destructor
+      virtual ~SpawnComponent();
+
+      virtual void CleanUp();
+
+   private:
+
+      typedef std::vector<dtCore::RefPtr<SpawnVolumeActorProxy> > SpawnVolumeArray;
+      SpawnVolumeArray mSpawnVolumes;
+
+   };
+}//namespace NetDemo
+
+#endif //NETDEMO_SPAWNCOMPONENT
