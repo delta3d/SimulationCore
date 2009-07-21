98978eca16010ade233cc9aa029aff2fa2d2d8f4
David Guthrie <LEgregius@users.noreply.github.com>
2009-07-21 14:06:29 +0000

    Put the SetDateTime log message to use a logger specific to the class.

--- source/SimCore/Actors/IGEnvironmentActor.cpp
+++ source/SimCore/Actors/IGEnvironmentActor.cpp
@@ -237,7 +237,7 @@ namespace SimCore
       /////////////////////////////////////////////////////////////
       void IGEnvironmentActor::SetDateTime(const dtUtil::DateTime& dt)
       {
-         dtUtil::Log::GetInstance().LogMessage(dtUtil::Log::LOG_DEBUG, __FILE__, "Sim time set to:%s",
+         dtUtil::Log::GetInstance("IGEnvironmentActor.cpp").LogMessage(dtUtil::Log::LOG_DEBUG, __FILE__, "Sim time set to:%s",
             dt.ToString(dtUtil::DateTime::TimeFormat::CALENDAR_DATE_AND_TIME_FORMAT).c_str());
 
          mEnvironment->SetDateTime(dt);
