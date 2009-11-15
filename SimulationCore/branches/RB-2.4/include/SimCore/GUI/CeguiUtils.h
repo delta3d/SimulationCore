940f92928cad0c995d45c57bd96fdf3d982b416d
crodgers <crodgers@df8a077f-1820-0410-8776-e0b621f843e8>
2009-11-15 07:12:51 +0000

    * Added a couple useful methods to the CEGIU Utils class. * Checked in the cpp that was forgotten on the initial check in of the header.

--- SimulationCore/branches/RB-2.4/include/SimCore/GUI/CeguiUtils.h
+++ SimulationCore/branches/RB-2.4/include/SimCore/GUI/CeguiUtils.h
@@ -21,15 +21,12 @@
 #include <osg/Vec4>
 #include <SimCore/Export.h>
 
+#ifdef None
+#undef None
+#endif
+#include <CEGUI.h>
 
 
-////////////////////////////////////////////////////////////////////////////////
-// FORWARD DECLARATIONS
-////////////////////////////////////////////////////////////////////////////////
-namespace CEGUI
-{
-   class Window;
-}
 
 namespace SimCore
 {
@@ -42,20 +39,96 @@ namespace SimCore
       {
          public:
             /**
-            * Get the absolute normalized OpenGL screen coordinates and size for
-            * a nested CEGUI window.
-            * NOTE: The CEGUI window must be at its final level of nesting in order
-            *       for this function to return a proper result.
-            * NOTE: CEGUI Y-Axis is +y down from top of screen while the returned
-            *       OpenGL coordinates are returned as +y up from the bottom of the screen.
-            */
+             * Get the absolute normalized OpenGL screen coordinates and size for
+             * a nested CEGUI window.
+             * NOTE: The CEGUI window must be at its final level of nesting in order
+             *       for this function to return a proper result.
+             * NOTE: CEGUI Y-Axis is +y down from top of screen while the returned
+             *       OpenGL coordinates are returned as +y up from the bottom of the screen.
+             */
             static osg::Vec4 GetNormalizedScreenBounds(const CEGUI::Window& window);
+
+            /**
+             * Get all windows of a specific type that are in the CEGUI window system
+             * or that are children of a specified root window.
+             * NOTE: this method may be performance heavy, so it is advized to
+             * call this method only when absolutely necessary.
+             * @param rootWindow Window that the found windows should be children of.
+             *        If left NULL, all windows in the system of the specified type will
+             *        be returned.
+             * @param outChildWindows List in which to capture the found windows.
+             * @return Number of windows that were found.
+             */
+            template<class T_CeguiWin>
+            static int GetChildWindowsByType(CEGUI::Window* rootWindow,
+               std::vector<T_CeguiWin*>& outChildWindows);
+
+            /**
+             * Convenience method for determining if a window is truly a child of another.
+             * @param parent Window in question about being the parent to "child".
+             * @param child Window in question about being a child of "parent".
+             * @return TRUE if "child" is a child window of "parent".
+             */
+            static bool IsParentAndChild(const CEGUI::Window& parent, const CEGUI::Window& child);
 
          private:
             // This should not be instantiated.
             CeguiUtils();
             ~CeguiUtils();
       };
+
+
+
+      //////////////////////////////////////////////////////////////////////////
+      // TEMPLATE METHOD DEFINITIONS
+      //////////////////////////////////////////////////////////////////////////
+      template<class T_CeguiWin>
+      int CeguiUtils::GetChildWindowsByType(CEGUI::Window* rootWindow,
+         std::vector<T_CeguiWin*>& outChildWindows)
+      {
+         int successes = 0;
+
+         // Get ready to loop through all the windows in the CEGUI system.
+         T_CeguiWin* curWin = NULL;
+         CEGUI::WindowManager::WindowIterator winIter = CEGUI::WindowManager::getSingleton().getIterator();
+         winIter.toStart();
+
+         // Search all the windows in the window manager.
+         while( ! winIter.isAtEnd())
+         {
+            curWin = dynamic_cast<T_CeguiWin*>(*winIter);
+
+            // If the window if of the specified type...
+            if(curWin != NULL)
+            {
+               // ...add the window to the list only if it is a child
+               // of the specified root window.
+               if(rootWindow != NULL)
+               {
+                  if(CeguiUtils::IsParentAndChild(*rootWindow, *curWin))
+                  {
+                     printf("\n");
+                     printf(curWin->getName().c_str());
+                     printf("\n");
+                     outChildWindows.push_back(curWin);
+                     ++successes;
+                  }
+               }
+               else // No root was specified...
+               {
+                  // ...simply add the window to the list since no root
+                  // was specified.
+                  outChildWindows.push_back(curWin);
+                  ++successes;
+               }
+            }
+
+            // Get ready for next loop.
+            ++winIter;
+         }
+
+         return successes;
+      }
 
    } // END - GUI namespace
 } // END - SimCore namespace
