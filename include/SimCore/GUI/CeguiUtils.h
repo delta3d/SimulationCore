3b5ba4d9d9e0a4c6a47738700a4a3b17849bad1f
crodgers <crodgers@df8a077f-1820-0410-8776-e0b621f843e8>
2009-11-16 04:01:11 +0000

    CEGUI Utils: * Added a filter class. * Modified the GetChildWindowsByType method parameter list and code to take a filter object instance.
    NetDemo LookNFeel:
    * Removed unused parts of the List Item definition.
    * Added Meter definition and registered it with the scheme file.
    * Added TabIndex property to the Button definition.
    
    GUI Component:
    * Added a button filter.
    * Fixed the button highlight visibility toggle.

--- include/SimCore/GUI/CeguiUtils.h
+++ include/SimCore/GUI/CeguiUtils.h
@@ -39,6 +39,44 @@ namespace SimCore
       {
          public:
             /**
+             * Filter object, that acts like a predicate, that determines the conditions
+             * under which a window can be accepted during a search operation.
+             * This class is intended to be sub-classed to override the Accept method.
+             */
+            template<typename T_CeguiWin>
+            class CeguiWindowFilter
+            {
+               public:
+                  /**
+                   * Override method.
+                   * Method that determines if the specified window is acceptable.
+                   * @param window The window to be checked.
+                   * @return TRUE if the window is acceptable.
+                   */
+                  virtual bool Accept(const T_CeguiWin& window)
+                  {
+                     return true;
+                  }
+            };
+
+            /**
+             * Get all windows of a specific type that are in the CEGUI window system
+             * or that are children of a specified root window.
+             * NOTE: this method may be performance heavy, so it is advized to
+             * call this method only when absolutely necessary.
+             * @param outChildWindows List in which to capture the found windows.
+             * @param rootWindow Window that the found windows should be children of.
+             *        If left NULL, all windows in the system of the specified type will
+             *        be returned.
+             * @param filter Pointer to an object that  determines the conditions under
+             *        which a window can be added to the outChildWindows list.
+             * @return Number of windows that were found.
+             */
+            template<typename T_CeguiWin>
+            static int GetChildWindowsByType(std::vector<T_CeguiWin*>& outChildWindows,
+               const CEGUI::Window* rootWindow, CeguiWindowFilter<T_CeguiWin>* filter = NULL);
+
+            /**
              * Get the absolute normalized OpenGL screen coordinates and size for
              * a nested CEGUI window.
              * NOTE: The CEGUI window must be at its final level of nesting in order
@@ -47,21 +85,6 @@ namespace SimCore
              *       OpenGL coordinates are returned as +y up from the bottom of the screen.
              */
             static osg::Vec4 GetNormalizedScreenBounds(const CEGUI::Window& window);
-
-            /**
-             * Get all windows of a specific type that are in the CEGUI window system
-             * or that are children of a specified root window.
-             * NOTE: this method may be performance heavy, so it is advized to
-             * call this method only when absolutely necessary.
-             * @param rootWindow Window that the found windows should be children of.
-             *        If left NULL, all windows in the system of the specified type will
-             *        be returned.
-             * @param outChildWindows List in which to capture the found windows.
-             * @return Number of windows that were found.
-             */
-            template<class T_CeguiWin>
-            static int GetChildWindowsByType(CEGUI::Window* rootWindow,
-               std::vector<T_CeguiWin*>& outChildWindows);
 
             /**
              * Convenience method for determining if a window is truly a child of another.
@@ -82,9 +105,9 @@ namespace SimCore
       //////////////////////////////////////////////////////////////////////////
       // TEMPLATE METHOD DEFINITIONS
       //////////////////////////////////////////////////////////////////////////
-      template<class T_CeguiWin>
-      int CeguiUtils::GetChildWindowsByType(CEGUI::Window* rootWindow,
-         std::vector<T_CeguiWin*>& outChildWindows)
+      template<typename T_CeguiWin>
+      int CeguiUtils::GetChildWindowsByType(std::vector<T_CeguiWin*>& outChildWindows,
+         const CEGUI::Window* rootWindow, CeguiWindowFilter<T_CeguiWin>* filter)
       {
          int successes = 0;
 
@@ -94,6 +117,7 @@ namespace SimCore
          winIter.toStart();
 
          // Search all the windows in the window manager.
+         bool acceptWindow = true;
          while( ! winIter.isAtEnd())
          {
             curWin = dynamic_cast<T_CeguiWin*>(*winIter);
@@ -101,25 +125,31 @@ namespace SimCore
             // If the window if of the specified type...
             if(curWin != NULL)
             {
-               // ...add the window to the list only if it is a child
-               // of the specified root window.
-               if(rootWindow != NULL)
+               // Filter the current window if a filter was specified.
+               if(filter != NULL)
                {
-                  if(CeguiUtils::IsParentAndChild(*rootWindow, *curWin))
+                  acceptWindow = filter->Accept(*curWin);
+               }
+
+               if(acceptWindow)
+               {
+                  // ...add the window to the list only if it is a child
+                  // of the specified root window.
+                  if(rootWindow != NULL)
                   {
-                     printf("\n");
-                     printf(curWin->getName().c_str());
-                     printf("\n");
+                     if(CeguiUtils::IsParentAndChild(*rootWindow, *curWin))
+                     {
+                        outChildWindows.push_back(curWin);
+                        ++successes;
+                     }
+                  }
+                  else // No root was specified...
+                  {
+                     // ...simply add the window to the list since no root
+                     // was specified.
                      outChildWindows.push_back(curWin);
                      ++successes;
                   }
-               }
-               else // No root was specified...
-               {
-                  // ...simply add the window to the list since no root
-                  // was specified.
-                  outChildWindows.push_back(curWin);
-                  ++successes;
                }
             }
 
