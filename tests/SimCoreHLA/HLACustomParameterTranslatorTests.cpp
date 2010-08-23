6d477964a26c0dbd69827491e5be441fd7784e41
crodgers <crodgers@df8a077f-1820-0410-8776-e0b621f843e8>
2010-08-23 19:31:33 +0000

    * Fixed an obvious bug in the parameter translator. * Added unit tests that appear not to have existed for this class.

--- tests/SimCoreHLA/HLACustomParameterTranslatorTests.cpp
+++ tests/SimCoreHLA/HLACustomParameterTranslatorTests.cpp
@@ -35,6 +35,7 @@
 #include <dtHLAGM/attributetoproperty.h>
 #include <dtHLAGM/parametertoparameter.h>
 #include <dtHLAGM/onetoonemapping.h>
+#include <dtHLAGM/distypes.h>
 
 #include <SimCore/HLA/HLACustomParameterTranslator.h>
 
@@ -52,6 +53,7 @@ namespace SimCore
          CPPUNIT_TEST_SUITE(HLACustomParameterTranslatorTests);
 
          CPPUNIT_TEST(TestTimeConversion);
+         CPPUNIT_TEST(TestVec3Conversion);
 
          CPPUNIT_TEST_SUITE_END();
 
@@ -106,6 +108,7 @@ namespace SimCore
                // size must be passed in by reference
                size_t size = 4U;
                mTranslator->MapFromMessageParameters(buffer, size, parameters, otm);
+               CPPUNIT_ASSERT_EQUAL(4U, unsigned(size));
 
                // the datastream will delete the buffer.
                dtUtil::DataStream ds(buffer, 4U, true);
@@ -113,7 +116,103 @@ namespace SimCore
                ds.Read(timeMillis);
 
                CPPUNIT_ASSERT_EQUAL(43433250L, timeMillis);
-               CPPUNIT_ASSERT_EQUAL(4U, unsigned(size));
+            }
+            catch (const dtUtil::Exception& ex)
+            {
+               CPPUNIT_FAIL(ex.What());
+            }
+         }
+
+         void TestVec3Conversion()
+         {
+            dtHLAGM::OneToManyMapping otmD("3Doubles", HLACustomAttributeType::VEC3D_TYPE, false ,false);
+            dtHLAGM::OneToManyMapping otmF("3Floats", HLACustomAttributeType::VEC3F_TYPE, false ,false);
+            dtHLAGM::OneToManyMapping::ParameterDefinition pdV3D("testVec3Double", dtDAL::DataType::VEC3D, "", false);
+            dtHLAGM::OneToManyMapping::ParameterDefinition pdV3F("testVec3Float", dtDAL::DataType::VEC3F, "", false);
+            otmD.GetParameterDefinitions().push_back(pdV3D);
+            otmF.GetParameterDefinitions().push_back(pdV3F);
+
+            dtCore::RefPtr<dtGame::Vec3dMessageParameter> vecDoubleParam = new dtGame::Vec3dMessageParameter("testVec3Double");
+            dtCore::RefPtr<dtGame::Vec3fMessageParameter> vecFloatParam = new dtGame::Vec3fMessageParameter("testVec3Float");
+
+            double epsilon = 0.001;
+
+            const size_t D_BUFFER_SIZE = sizeof(double) * 3;
+            const size_t F_BUFFER_SIZE = sizeof(float) * 3;
+            char* bufferD = new char[D_BUFFER_SIZE];
+            char* bufferF = new char[F_BUFFER_SIZE];
+            osg::Vec3d testValueD(1234.56789, 9876.54321, -0.123456789);
+            osg::Vec3f testValueF(10.11f, -121.314f, 1516.17f);
+            try
+            {
+               std::vector<dtCore::RefPtr<dtGame::MessageParameter> > parametersD;
+               std::vector<dtCore::RefPtr<dtGame::MessageParameter> > parametersF;
+               parametersD.push_back(vecDoubleParam);
+               parametersF.push_back(vecFloatParam);
+
+               // the datastream will delete the buffer.
+               dtUtil::DataStream dsD(bufferD, D_BUFFER_SIZE, true);
+               dtUtil::DataStream dsF(bufferF, F_BUFFER_SIZE, true);
+               dsD.Write(testValueD);
+               dsF.Write(testValueF);
+
+               mTranslator->MapToMessageParameters(bufferD, D_BUFFER_SIZE, parametersD, otmD);
+               mTranslator->MapToMessageParameters(bufferF, F_BUFFER_SIZE, parametersF, otmF);
+
+               CPPUNIT_ASSERT_DOUBLES_EQUAL(testValueD.x(), vecDoubleParam->GetValue().x(), epsilon);
+               CPPUNIT_ASSERT_DOUBLES_EQUAL(testValueD.y(), vecDoubleParam->GetValue().y(), epsilon);
+               CPPUNIT_ASSERT_DOUBLES_EQUAL(testValueD.z(), vecDoubleParam->GetValue().z(), epsilon);
+               CPPUNIT_ASSERT_DOUBLES_EQUAL(testValueF.x(), vecFloatParam->GetValue().x(), epsilon);
+               CPPUNIT_ASSERT_DOUBLES_EQUAL(testValueF.y(), vecFloatParam->GetValue().y(), epsilon);
+               CPPUNIT_ASSERT_DOUBLES_EQUAL(testValueF.z(), vecFloatParam->GetValue().z(), epsilon);
+            }
+            catch (const dtUtil::Exception& ex)
+            {
+               CPPUNIT_FAIL(ex.What());
+            }
+
+            bufferD = new char[D_BUFFER_SIZE];
+            bufferF = new char[F_BUFFER_SIZE];
+            osg::Vec3d testValueD2(9.8, -76.54, 321.1011);
+            osg::Vec3f testValueF2(-12.34f, 876.5f, 1011.9f);
+            try
+            {
+               vecDoubleParam->SetValue(testValueD2);
+               vecFloatParam->SetValue(testValueF2);
+               std::vector<dtCore::RefPtr<const dtGame::MessageParameter> > parametersD;
+               std::vector<dtCore::RefPtr<const dtGame::MessageParameter> > parametersF;
+               parametersD.push_back(vecDoubleParam);
+               parametersF.push_back(vecFloatParam);
+
+               // Buffer size must be passed in by reference
+               size_t bufferSize = D_BUFFER_SIZE;
+               mTranslator->MapFromMessageParameters(bufferD, bufferSize, parametersD, otmD);
+               // Verify that the max size equals the expected size.
+               CPPUNIT_ASSERT_EQUAL(D_BUFFER_SIZE, bufferSize);
+               // The datastream will delete the buffer.
+               dtUtil::DataStream dsD(bufferD, bufferSize, true);
+               mTranslator->MapFromMessageParameters(bufferD, bufferSize, parametersD, otmD);
+
+               bufferSize = F_BUFFER_SIZE;
+               mTranslator->MapFromMessageParameters(bufferF, bufferSize, parametersF, otmF);
+               // Verify that the max size equals the expected size.
+               CPPUNIT_ASSERT_EQUAL(F_BUFFER_SIZE, bufferSize);
+               // The datastream will delete the buffer.
+               dtUtil::DataStream dsF(bufferF, bufferSize, true);
+               mTranslator->MapFromMessageParameters(bufferF, bufferSize, parametersF, otmF);
+
+               // Read from the data streams
+               osg::Vec3d resultD;
+               osg::Vec3f resultF;
+               dsD.Read(resultD);
+               dsF.Read(resultF);
+
+               CPPUNIT_ASSERT_DOUBLES_EQUAL(testValueD2.x(), resultD.x(), epsilon);
+               CPPUNIT_ASSERT_DOUBLES_EQUAL(testValueD2.y(), resultD.y(), epsilon);
+               CPPUNIT_ASSERT_DOUBLES_EQUAL(testValueD2.z(), resultD.z(), epsilon);
+               CPPUNIT_ASSERT_DOUBLES_EQUAL(testValueF2.x(), resultF.x(), epsilon);
+               CPPUNIT_ASSERT_DOUBLES_EQUAL(testValueF2.y(), resultF.y(), epsilon);
+               CPPUNIT_ASSERT_DOUBLES_EQUAL(testValueF2.z(), resultF.z(), epsilon);
             }
             catch (const dtUtil::Exception& ex)
             {
