c0b74df6fa4c373023fc29cd8710f0d9e063bc0c
banderegg <banderegg@df8a077f-1820-0410-8776-e0b621f843e8>
2010-11-12 18:06:35 +0000

    +added documentation to volume rendering component and changed some default parameters

--- include/SimCore/Components/VolumeRenderingComponent.h
+++ include/SimCore/Components/VolumeRenderingComponent.h
@@ -45,13 +45,21 @@ namespace SimCore
 {
    namespace Components
    {
+      /**
+       * The volume rendering component allows the user to create simple shapes (sphere, box, etc.) using the internal ShapeVolumeRecord class
+       * which simulate volumes of fog or clouds (or by overriding the shader any sort of volume) by randomly placing particles within the volume
+       * that are billboards rendered as soft particles.
+       */
 
       class SIMCORE_EXPORT VolumeRenderingComponent : public dtGame::GMComponent
       {
       public:
 
          typedef dtGame::GMComponent BaseClass;
+         //the name of the component used to look up by name on the game manager
          static const std::string DEFAULT_NAME;
+
+         //these are the shader uniforms used in the default volumetric shaders
          static const std::string VOLUME_PARTICLE_POS_UNIFORM;
          static const std::string VOLUME_PARTICLE_COLOR_UNIFORM;
          static const std::string VOLUME_PARTICLE_INTENSITY_UNIFORM; 
@@ -62,10 +70,15 @@ namespace SimCore
          //static const std::string CAMERA_LINEAR_DEPTH_UNIFORM;
 
 
+         //a typedef for the id used by the shape volume record class
          typedef unsigned ShapeRecordID;
 
+         //the shape type enumeration, set on the shape volume record class
          enum Shape{SPHERE, ELLIPSOID, BOX, CONE, CYLINDER};
 
+         //the render mode is either SIMPLE_SHAPE_GEOMETRY or PARTICLE_VOLUME
+         //the simple shape mode just uses an osg shape drawable and is mostly used for debugging
+         //the particle volume randomly creates soft particles to fill the volume
          enum RenderMode{SIMPLE_SHAPE_GEOMETRY, PARTICLE_VOLUME};//, VOLUMETRIC_RAYCASTING};
 
          class ShapeVolumeRecord;
@@ -117,43 +130,112 @@ namespace SimCore
            public:
             ShapeVolumeRecord();
 
+            //this is the id used to look up the volumes, alternatively one can hold onto a ref or observer pointer
             ShapeRecordID mId;
+            
+            //the shape type determines the shape and dictates how the radius params are interpreted
+            //the shape types available are SPHERE, ELLIPSOID, BOX, CONE, and CYLINDER
             Shape mShapeType;
+            
+            //the render mode is either SIMPLE_SHAPE_GEOMETRY or PARTICLE_VOLUME
+            //the simple shape mode just uses an osg shape drawable and is mostly used for debugging
+            //the particle volume randomly creates soft particles to fill the volume
             RenderMode mRenderMode;
 
             //set the dirty flag to true if the color, radius, velocity, or intensity changes
             bool mDirtyParams;
+
+            //this flag is used internally to delete the shape volume on the next tick but can be set externally as well
             bool mDeleteMe;
-            bool mAutoDeleteAfterMaxTime; //using this flag will set the volume to be automatically removed after the number of seconds
-            float mMaxTime;
-            bool mFadeOut;
-            float mFadeOutTime;
-            float mIntensity;
-            float mDensity;
-            float mVelocity;
-            float mNoiseScale;
 
-            //these variables are only for the particle volume types
-            unsigned mNumParticles;
-            float mParticleRadius;
-
-            osg::Vec4 mColor;
-            osg::Vec3 mPosition;
-            osg::Vec3 mRadius;
-            osg::Vec3 mScale;
-            osg::Matrix mTransform;
-
+            //setting this flag to true will automatically remove the volume (after fade out time if it has one) if the 
+            //mTarget is deleted and the observer pointer is set to null
             bool mAutoDeleteOnTargetNull;
 
+            //using this flag will set the volume to be automatically removed after the number of seconds
+            bool mAutoDeleteAfterMaxTime; 
+
+            //use this value to set the max amount of time this shape volume will live 
+            float mMaxTime;
+
+            //setting this flag will fade out the voltume when the max time is reached or parent is NULL and the 
+            //delete flag is set, the amount of time to fade out is determined by the parameter below
+            bool mFadeOut;
+
+            //see above- to use this fade out time you must set mFadeOut (above) to true, this param is used to
+            //scale the intensity or alpha value down to zero over the amount of time
+            float mFadeOutTime;
+
+            //the intensity is simply a multiplier on the alpha value of the effect, it is used to allow the effect
+            //to fade in or out as necessary
+            float mIntensity;
+
+            //the density of the effect is used to determine the opacity using an exponential fog equation
+            float mDensity;
+
+            //the velocity is a simple scalar value used to animate the noise, or turbulence of the effect
+            //typically a value between 0.25 and 0.5 will be sufficient
+            float mVelocity;
+
+            //the noise scale value is used to scale the texture coordinates when looking up into a noise texture
+            //a large number will make the effect more granular but patterns will be more apparent
+            //a number between 0.5-1.5 is typical
+            float mNoiseScale;
+
+            //this parameter determines the number of particles to use for the effect, currently the max number is 150
+            //but can easily be changed by editing the constant max number in the shader
+            unsigned mNumParticles;
+
+            //the particle radius is used to determine how big each particle should be, typically around 5-20
+            //if you are unsure how large the particles should be to fill the volume you can call
+            //ComputeParticleRadius() on the component and it will compute a particle radius provided that the num particles
+            //and volume radius params are set, also make sure to add the volume before computing the radius otherwise the uniforms
+            //will not be set
+            float mParticleRadius;
+
+            //the color of the particles, the 4th component is used as a multiplier for the final alpha
+            //note that the alpha provided here, the intensity, and the density are all multiplied in to get the
+            //final alpha, so an alpha value of 1 here is usually sufficient
+            osg::Vec4 mColor;
+
+            //this is the position of the center of the volume with respect to its parent transformable, if there is no transformable parent
+            //then this position is in world space
+            osg::Vec3 mPosition;
+            
+            //the radius is used to determine the dimension and is interpreted differently for each shape
+            //SPHERE- mRadius[0] = outer radius, mRadius[1] = inner radius
+            //ELLIPSOID- mRadius is equal to the extents in each direction currently the same as a BOX
+            //BOX- mRadius is the extents in each direction
+            //CONE- mRadius[0] = radius at end of cone, mRadius[1] = height
+            //CYLINDER- mRadius[0] = radius, mRadius[1] = height
+            osg::Vec3 mRadius;
+            
+            //this matrix is used internally and will be overwritten each frame, not intended for use
+            //to apply a matrix use a transformable by setting the mTarget
+            osg::Matrix mTransform;
+
+            //the shader group and name refer to a specific shader in the ShaderManager shaderdef.xml file and
+            //is only necessary to override the default shaders applied which simulate a cloud/fog effect
             std::string mShaderGroup;
             std::string mShaderName;
+
+            //the mParentNode is used internally as a parent to the drawable, do not try to set this
             dtCore::ObserverPtr<osg::MatrixTransform> mParentNode; 
+
+            //the mTarget is used like a parent to the effect, setting this will allow your volume to move with
+            //another node, if mAutoDeleteOnTargetNull is true then this volume will be removed when the target is set to null
             dtCore::ObserverPtr<dtCore::Transformable> mTarget;
+
+            //this is the osg shape and is not used unless the render mode is SIMPLE_SHAPE_GEOMETRY
+            //this is created for you by the component
             dtCore::RefPtr<osg::Shape> mShape;
 
-            //this variables is only for the particle volume types
+            //this variables is only for the particle volume types and holds onto the actual osg drawable 
+            //used to render the volumes
+            //this is created for you by the component
             dtCore::RefPtr<ParticleVolumeDrawable> mParticleDrawable;
 
+            //this is a static counter used internally to give each volume an id
             static OpenThreads::Atomic mCounter;
          };
 
@@ -180,20 +262,37 @@ namespace SimCore
 
          virtual void OnRemovedFromGM();
 
+         //this function is used to add a shape volume to the component using a pre-created shape volume record class
+         //see the shape record volume class above to figure out what params to set
          ShapeRecordID CreateShapeVolume(dtCore::RefPtr<ShapeVolumeRecord> svr);
+
+         //just a helper class that will create a shape volume for you with minimal parameters
          ShapeRecordID CreateStaticShapeVolume(Shape s, const osg::Vec4& color, const osg::Vec3& center, const osg::Vec3& radius);
 
+         //used to remove a shape volume from the system
          void RemoveShapeVolume(ShapeRecordID id);
 
+         //used to find a shape volume by Id
          ShapeVolumeRecord* FindShapeVolumeById(ShapeRecordID id);
+
+         //this function will find a shape volume whose target has the provided actor id, useful for finding a volume
+         //attached to a specific actor
          ShapeVolumeRecord* FindShapeVolumeForActor(const dtCore::UniqueId& actorID);
-         //void FindAllShapeVolumesForActor(const dtCore::UniqueId& actorID, std::vector<ShapeVolumeRecord*> pContainerToFill);
 
-         void CreateDepthPrePass(unsigned width, unsigned height);
-
+         //a volume can be moved by setting the mPosition and updatingt the dirty flag, but this is a helper
+         //function which will move the volume for you
          void MoveVolume(ShapeVolumeRecord& svr, const osg::Vec3& moveBy);
+
+         //this function should be used to resize the radius of a specific volume, useful for a growing effect if called per frame
+         //the simple moving shape actor uses this when dead reckoning the size
          void ExpandVolume(ShapeVolumeRecord& svr, const osg::Vec3& expandBy);
+
+         //a helper function which uses the number of particles and total volume of shape to compute a particle radius
+         //this function is useful if you are using the expand function above and need a computed particle radius
          void ComputeParticleRadius(ShapeVolumeRecord& svr);
+
+         //a function used internally to create a linear pre depth pass
+         void CreateDepthPrePass(unsigned width, unsigned height);
 
       protected:
 
