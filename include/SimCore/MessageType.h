e3fc3b893f16420c560cf9bcc8def7f94a8dc3fe
David Guthrie <LEgregius@users.noreply.github.com>
2011-04-15 17:25:48 +0000

    Added the terrain loaded message, also added a convenience function to call preframe with an arbitratry value on a gm instance.

--- include/SimCore/MessageType.h
+++ include/SimCore/MessageType.h
@@ -58,6 +58,8 @@ namespace SimCore
       static const MessageType TIME_QUERY;
       static const MessageType TIME_VALUE;
 
+      static const MessageType INFO_TERRAIN_LOADED;
+
       // These are NON-CONST because they are used in an actor property
       static MessageType BINOCULARS;
       static MessageType COMPASS;
--- source/SimCore/Actors/TerrainActorProxy.cpp
+++ source/SimCore/Actors/TerrainActorProxy.cpp
@@ -26,6 +26,7 @@
 #include <SimCore/CollisionGroupEnum.h>
 #include <SimCore/Actors/PagedTerrainPhysicsActor.h>
 #include <SimCore/Actors/EntityActorRegistry.h>
+#include <SimCore/MessageType.h>
 #include <dtDAL/enginepropertytypes.h>
 #include <dtDAL/actorproxyicon.h>
 #include <dtDAL/project.h>
@@ -445,16 +446,20 @@ namespace SimCore
             //Set the helper name to match the actor name.
             mHelper->SetName(GetName());
          }
+
+         dtCore::RefPtr<dtGame::Message> terrainMessage;
+         GetGameActorProxy().GetGameManager()->GetMessageFactory().CreateMessage(SimCore::MessageType::INFO_TERRAIN_LOADED, terrainMessage);
+         terrainMessage->SetAboutActorId(GetUniqueId());
+         GetGameActorProxy().GetGameManager()->SendMessage(*terrainMessage);
       }
 
       /////////////////////////////////////////////////////////////////////////////
       void TerrainActor::LoadMeshFromFile(const std::string& fileToLoad, const std::string& materialType)
       {
-         if(dtUtil::FileUtils::GetInstance().FileExists(fileToLoad))
+         if (dtUtil::FileUtils::GetInstance().FileExists(fileToLoad))
          {
-
             std::string filename = dtUtil::FindFileInPathList(fileToLoad);
-            if(!filename.empty())
+            if (!filename.empty())
             {
                dtPhysics::PhysicsReaderWriter::PhysicsTriangleData data;
                data.mFaces = new osg::UIntArray();
@@ -582,8 +587,8 @@ namespace SimCore
       {
          if (!mLoadNodeTask.valid())
          {
-            // It's done, but didn't load anything.
-            return true;
+            // It's done already or nothing is happening.
+            return mTerrainNode.valid();
          }
 
          if (!mLoadNodeTask->IsComplete())
