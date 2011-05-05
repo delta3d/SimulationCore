e427954bf7fb25d356c63ea1035632dbdbedc46d
banderegg <banderegg@df8a077f-1820-0410-8776-e0b621f843e8>
2011-05-05 20:12:31 +0000

    +updating version

--- include/SimCore/SimCoreVersion.h
+++ include/SimCore/SimCoreVersion.h
@@ -38,9 +38,9 @@
 
 namespace SimCore
 {
-   std::string SIMCORE_SVN_REVISION("3192");
-   std::string SIMCORE_SVN_DATE("2011/04/11 10:46:17");
-   std::string SIMCORE_BUILD_DATE("2011/04/11 13:34:08");
+   std::string SIMCORE_SVN_REVISION("3247");
+   std::string SIMCORE_SVN_DATE("2011/05/05 15:44:33");
+   std::string SIMCORE_BUILD_DATE("2011/05/05 15:54:48");
 }
 
 #endif
