4ef3ad40f20853230bb63d7cc0baf375f7177edb
David Guthrie <LEgregius@users.noreply.github.com>
2013-01-18 17:41:47 +0000

    Fixed some pathing issues

--- CMakeExternals/Delta3D.cmake
+++ CMakeExternals/Delta3D.cmake
@@ -12,31 +12,32 @@
   SET(Delta3D_DEPENDS ${proj})
 
   IF(NOT DEFINED DELTA_DIR)
-    set(${proj}_SOURCE_DIR  ${CMAKE_BINARY_DIR})
     if (SUPER_BUILD_SOURCE_AT_TOP_LEVEL)
-      set(${proj}_SOURCE_DIR ${CMAKE_SOURCE_DIR}/..)
+      set(${proj}_SOURCE_DIR ${CMAKE_SOURCE_DIR}/../${proj} CACHE STRING "project source dir")
+    else()
+      set(${proj}_SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj} CACHE STRING "project source dir")
     endif()
 
-    set(${proj}_BINARY_MODE BINARY_DIR ${${proj}_SOURCE_DIR}/${proj}/build)
+    set(${proj}_BINARY_MODE BINARY_DIR ${${proj}_SOURCE_DIR}/build)
     if (SUPER_BUILD_IN_SOURCE)
       set(${proj}_BINARY_MODE BUILD_IN_SOURCE 1)
     endif()
 
      
-    SET(DELTA_DIR ${${proj}_SOURCE_DIR}/${proj})
+    SET(DELTA_DIR ${${proj}_SOURCE_DIR})
     SET(DELTA3D_INCLUDE_DIR ${DELTA_DIR}/inc)
     if (NOT SUPER_BUILD_IN_SOURCE)
-      SET(DELTA3D_LIB_DIR ${${proj}_SOURCE_DIR}/${proj}/build/lib)
+      SET(DELTA3D_LIB_DIR ${${proj}_SOURCE_DIR}/build/lib)
     else()
       SET(DELTA3D_LIB_DIR ${DELTA_DIR}/lib)
     endif()
      
-    SET(DELTA3D_EXT_DIR ${CMAKE_SOURCE_DIR}/ext)
+    SET(DELTA3D_EXT_DIR ${CMAKE_SOURCE_DIR}/ext CACHE STRING "delta3d external dependency dir")
 
     ExternalProject_Add(${proj}
       SVN_REPOSITORY https://delta3d.svn.sourceforge.net/svnroot/delta3d/trunk/delta3d
       ${${proj}_BINARY_MODE}
-      SOURCE_DIR ${${proj}_SOURCE_DIR}/${proj}
+      SOURCE_DIR ${${proj}_SOURCE_DIR}
       INSTALL_COMMAND ""
       CMAKE_GENERATOR ${gen}
       CMAKE_ARGS
@@ -52,7 +53,7 @@
         -DBUILD_3DSMAX_PLUGIN=${BUILD_3DSMAX_PLUGIN}
         -DAUTO_RUN_TESTS:BOOL=${AUTO_RUN_TESTS}
         -DDELTA3D_LIB_DIR:PATH=${DELTA3D_LIB_DIR}
-        -DDELTA3D_EXT_DIR:PATH=${CMAKE_SOURCE_DIR}/ext
+        -DDELTA3D_EXT_DIR:PATH=${DELTA3D_EXT_DIR}
       DEPENDS ${proj_DEPENDENCIES}
      )
      
